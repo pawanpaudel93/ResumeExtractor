5793c946a9d422de1928b2f6f7db7f829db43cdd
Pawan Paudel <pawanpaudel93@gmail.com>
2020-01-11 23:49:17 +0545

    getting ready for production env

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --port=$PORT ResumeExtractor.wsgi:application
--- ResumeExtractor/settings/production.py
+++ ResumeExtractor/settings/production.py
@@ -1,5 +1,6 @@
 import dj_database_url
 from decouple import config
+from .base import MIDDLEWARE
 
 DEBUG = False
 ALLOWED_HOSTS = ['*']
@@ -9,11 +10,13 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
 
-# # for hosting media files
-# # Do not change any of these names
-# B2_BUCKET_NAME = config('HB2_B2_BUCKET_NAME')
-# B2_BUCKET_ID = config('HB2_B2_BUCKET_ID')
-# B2_ACCOUNT_ID = config('HB2_B2_ACCOUNT_ID')
-# B2_APPLICATION_KEY = config('HB2_B2_APP_KEY')
-# DEFAULT_FILE_STORAGE = 'django_b2storage.backblaze_b2.B2Storage'
+# for hosting media files
+# Do not change any of these names
+B2_BUCKET_NAME = config('HB2_B2_BUCKET_NAME')
+B2_BUCKET_ID = config('HB2_B2_BUCKET_ID')
+B2_ACCOUNT_ID = config('HB2_B2_ACCOUNT_ID')
+B2_APPLICATION_KEY = config('HB2_B2_APP_KEY')
+DEFAULT_FILE_STORAGE = 'django_b2storage.backblaze_b2.B2Storage'
